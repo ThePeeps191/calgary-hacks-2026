5d54d6f746e706209f251a1421ddc92e048f027c
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2026-02-14 16:00:51 -0700

    replace example url

--- scraper/get_content.py
+++ scraper/get_content.py
@@ -30,5 +30,5 @@ def get_content(url):
     return content_dict
 
 if __name__ == "__main__":
-    content_dict = get_content("https://www.ctvnews.ca/calgary/article/in-court-charge-stayed-for-one-of-three-accused-of-bilking-bowness-non-profit/")
+    content_dict = get_content("https://edition.cnn.com/2023/10/29/sport/nfl-week-8-how-to-watch-spt-intl/index.html")
     print(content_dict)
