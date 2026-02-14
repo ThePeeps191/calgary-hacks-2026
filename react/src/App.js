1812aaf2fa727e9c183da7a388863348107d4e8b
Jonas Huang <jonaszchuang@gmail.com>
2026-02-14 16:06:40 -0700

    added user input

--- react/src/App.js
+++ react/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleSend = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:5000/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      });
+
+      const data = await res.json();
+      setOutput(data.result);
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Please put your text</p>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Text"
+        />
+        <button onClick={handleSend}>Check</button>
+
+        {output && (
+          <p>
+            <strong>Response:</strong> {output}
+          </p>
+        )}
       </header>
     </div>
   );
