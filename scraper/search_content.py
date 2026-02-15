2b92bcce6366e571ab8ac8896117d08892a3050b
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2026-02-15 08:26:57 -0700

    fix searching content

--- scraper/search_content.py
+++ scraper/search_content.py
@@ -1,48 +1,50 @@
-# import os
-# import requests
-# from dotenv import load_dotenv
+import os
+import requests
+from dotenv import load_dotenv
 
-# load_dotenv()
-# api_key = os.getenv("NEWSAPI_KEY")
+load_dotenv()
+api_key = os.getenv("NEWSAPI_KEY")
 
-# def search_urls(query, url):
-#     params = {
-#         "q": query,
-#         "sortBy": "publishedAt",
-#         "pageSize": 10,
-#         "apiKey": api_key,
-#         "language": "en"
-#     }
+def search_urls(query, url):
+    u = ('https://newsapi.org/v2/everything?'
+       f'q={query}&'
+       'from=2026-02-13&'
+       'sortBy=popularity&'
+       f'apiKey={api_key}')
+    response = requests.get(u)
+    print(response.text)
+    print(response.json())
 
-#     response = requests.get(url, params=params)
-
-#     if response.status_code == 200:
-#         data = response.json()
-#         articles = data.get("articles", [])
+    if response.status_code == 200:
+        data = response.json()
+        articles = data.get("articles", [])
         
-#         unique_articles = []
-#         seen_titles = set()
+        unique_articles = []
+        seen_titles = set()
 
-#         for article in articles:
-#             title = article['title'].strip().lower()
+        for article in articles:
+            title = article['title'].strip().lower()
             
-#             if title not in seen_titles:
-#                 seen_titles.add(title)
-#                 unique_articles.append(article)
+            if title not in seen_titles:
+                seen_titles.add(title)
+                unique_articles.append(article)
             
-#             if len(unique_articles) == 5:
-#                 break
+            if len(unique_articles) == 5:
+                break
 
-#         print(f"--- 5 Unique Latest Articles for '{query}' ---\n")
-#         for i, article in enumerate(unique_articles, 1):
-#             print(f"{i}. {article['title']}")
-#             print(f"   Published: {article['publishedAt']}")
-#             print(f"   Source: {article['source']['name']}")
-#             print(f"   Link: {article['url']}\n")
-            
-#     else:
-#         print(f"Error: {response.status_code}")
-#         print(response.json())
+        # print(f"--- 5 Unique Latest Articles for '{query}' ---\n")
+        ans = []
+        for i, article in enumerate(unique_articles, 1):
+            ans.append(article)
+        # for i, article in enumerate(unique_articles, 1):
+        #     print(f"{i}. {article['title']}")
+        #     print(f"   Published: {article['publishedAt']}")
+        #     print(f"   Source: {article['source']['name']}")
+        #     print(f"   Link: {article['url']}\n")
+        return ans
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.json())
 
-# # Call the function
-# search_urls("OpenAI", "https://www.wired.com/story/openai-nuking-4o-model-china-chatgpt-fans-arent-ok/")
+# Call the function
+# search_urls("openai", "https://www.wired.com/story/openai-nuking-4o-model-china-chatgpt-fans-arent-ok/")
